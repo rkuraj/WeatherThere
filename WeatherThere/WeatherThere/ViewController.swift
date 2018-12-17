7eabbc34a62b89ecae338088e6d58d581d4a57ef
rkuraj <kuraj.robert@gmail.com>
2018-12-17 22:25:59 +0100

    create project

---
+++ WeatherThere/WeatherThere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherThere
+//
+//  Created by Robert Kuraj on 17/12/2018.
+//  Copyright © 2018 Robert Kuraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
